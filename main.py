5121b0b809b8b45085cf2d6d11f853ee9ae0e539
hackerAman <13135277-hackerAman@users.noreply.replit.com>
2023-07-25 06:00:52 +0000

    clockwise and anticlockwise , both are happening.

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ def home():
 def transform():
     url = request.json['url']
 
-    # Get the Cloudinary configuration from environment variables
     cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME')
     api_key = os.getenv('CLOUDINARY_API_KEY')
     api_secret = os.getenv('CLOUDINARY_API_SECRET')
@@ -26,18 +25,22 @@ def transform():
       api_secret = api_secret  
     )
 
-
     try:
         upload_result = cloudinary.uploader.upload(url, resource_type="video")
         width = upload_result['width']
         height = upload_result['height']
-        transformation = {"angle": -90} if width > height else {}
-        result = cloudinary.uploader.upload(url, resource_type="video", **transformation)
 
-        https_url = result['url'].replace("http://", "https://")  # Replace http with https
-        return {'transformedUrl': https_url}
+        # Do two transformations
+        result_anticlockwise = cloudinary.uploader.upload(url, resource_type="video", angle=-90) if width > height else upload_result
+        result_clockwise = cloudinary.uploader.upload(url, resource_type="video", angle=90) if width > height else upload_result
+
+        https_url_anticlockwise = result_anticlockwise['url'].replace("http://", "https://")
+        https_url_clockwise = result_clockwise['url'].replace("http://", "https://")
+
+        return {'transformedUrlAnticlockwise': https_url_anticlockwise, 'transformedUrlClockwise': https_url_clockwise}
     except Exception as e:
         return {'error': str(e)}, 500
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=81)
